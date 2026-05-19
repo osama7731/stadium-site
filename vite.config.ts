44b91c90228914d7580ca8e23b485745023ca8e7
osama7731 <abd7731000551234@gmail.com>
2026-05-19 07:42:14 +0300

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-
+  plugins: [react()],
   build: {
     outDir: "dist",
   },
-})
+});
